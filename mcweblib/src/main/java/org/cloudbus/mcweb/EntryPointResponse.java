b92408107668a26e3e241c794f81795cc37d2613
Nikolay Grozev <nikolay.grozev@gmail.com>
2015-06-11 23:42:05 +1000

    :Updated# Please enter the commit message for your changes. Lines starting

--- mcweblib/src/main/java/org/cloudbus/mcweb/EntryPointResponse.java
+++ mcweblib/src/main/java/org/cloudbus/mcweb/EntryPointResponse.java
@@ -3,41 +3,42 @@ package org.cloudbus.mcweb;
 import org.cloudbus.mcweb.DataCentre;
 
 public class EntryPointResponse {
-	
-	private String selectedCloudSiteCode;
-	private String redirectAddress;
-	private DataCentre definition;
-	
-	public EntryPointResponse(final String selectedCloudSiteCode, final String redirectAddress) {
-		this.selectedCloudSiteCode = selectedCloudSiteCode;
-		this.redirectAddress = redirectAddress;
-	}
-	
-	public EntryPointResponse(final String name, final String loadBalancerAddress, final DataCentre definition) {
-		this.definition = definition;
-	}
 
-	public String getSelectedCloudSiteCode() {
-		return selectedCloudSiteCode;
-	}
-	
-	public void setSelectedCloudSiteCode(String selectedCloudSiteCode) {
-		this.selectedCloudSiteCode = selectedCloudSiteCode;
-	}
-	
-	public String getRedirectAddress() {
-		return redirectAddress;
-	}
-	
-	public void setRedirectAddress(String redirectAddress) {
-		this.redirectAddress = redirectAddress;
-	}
+    private String selectedCloudSiteCode;
+    private String redirectAddress;
+    private DataCentre definition;
 
-	public DataCentre getDefinition() {
-		return definition;
-	}
+    public EntryPointResponse(final String selectedCloudSiteCode, final String redirectAddress) {
+        this.selectedCloudSiteCode = selectedCloudSiteCode;
+        this.redirectAddress = redirectAddress;
+    }
 
-	public void setDefinition(DataCentre definition) {
-		this.definition = definition;
-	}
+    public EntryPointResponse(final String name, final String loadBalancerAddress, final DataCentre definition) {
+        this(name, loadBalancerAddress);
+        this.definition = definition;
+    }
+
+    public String getSelectedCloudSiteCode() {
+        return selectedCloudSiteCode;
+    }
+
+    public void setSelectedCloudSiteCode(String selectedCloudSiteCode) {
+        this.selectedCloudSiteCode = selectedCloudSiteCode;
+    }
+
+    public String getRedirectAddress() {
+        return redirectAddress;
+    }
+
+    public void setRedirectAddress(String redirectAddress) {
+        this.redirectAddress = redirectAddress;
+    }
+
+    public DataCentre getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(DataCentre definition) {
+        this.definition = definition;
+    }
 }
